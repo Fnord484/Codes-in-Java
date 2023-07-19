c8929145b99a299a36f46325da8c0c9f2b7b9760
Fnord484 <106024845+Fnord484@users.noreply.github.com>
2023-07-19 17:43:34 -0300

    Create Area_Retangulo.java
    Signed-off-by: Fnord484 <106024845+Fnord484@users.noreply.github.com>

---
+++ Area_Retangulo.java
@@ -0,0 +1,19 @@
+package programinhas;
+
+public class calculandoArea_1{ //Área de um retângulo
+    
+public static void main(String[] args){
+    
+   double base, altura, areaRetangulo;
+   
+   base = 15.5;
+   altura = 2;
+   areaRetangulo = base * altura;
+   
+   System.out.println("A área de um retângulo de base "+ base +" metros");
+   System.out.print("e com altura de "+ altura +" metros");
+   System.out.println("\nSerá igual a "+areaRetangulo+" metros cúbicos");
+}
+
+
+}
